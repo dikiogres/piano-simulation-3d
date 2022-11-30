8ddf8bd8181c8fcd8630c5d1fb5657618dd88215
dikiogres <dikiogres@gmail.com>
2022-12-01 00:00:38 +0700

    feat: flatKeys position

--- src/lib/Piano.ts
+++ src/lib/Piano.ts
@@ -10,6 +10,17 @@ export default class Piano {
 
     constructor() {
         this.flatKeys = [
+            new Key('Db3', '2', 5),
+            new Key('Eb3', '3', 15),
+            new Key('Gb3', '5', 35),
+            new Key('Ab3', '6', 45),
+            new Key('Bb3', '7', 55),
+      
+            new Key('Db4', 's', 75),
+            new Key('Eb4', 'd', 85),
+            new Key('Gb4', 'g', 105),
+            new Key('Ab4', 'h', 115),
+            new Key('Bb4', 'j', 125),
 
         ];
       
