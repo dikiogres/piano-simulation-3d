6cd9ebea6a83e7b51fb500d286d6fc79016f126b
dikiogres <dikiogres@gmail.com>
2022-11-30 23:50:14 +0700

    feat: import key

--- src/lib/Piano.ts
+++ src/lib/Piano.ts
@@ -1,10 +1,22 @@
 import * as THREE from 'three';
 
+import Key from './Key';
+
 export default class Piano {
 
     private pianoGroup: any;
+    private flatKeys: any;
+    private naturalKeys: any;
 
     constructor() {
+        this.flatKeys = [
+
+        ];
+      
+          this.naturalKeys = [
+
+        ];
+
         this.pianoGroup = new THREE.Group(); 
     }
 
