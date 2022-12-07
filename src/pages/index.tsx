52bef65e4afdb7e55c5aaf1b7952487096c356df
dikiogres <dikiogres@gmail.com>
2022-12-08 00:51:33 +0700

    ...

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,64 +7,94 @@ import SceneInit from '../lib/SceneInit';
 import Piano from '../lib/Piano';
 
 function Home() {
-  
+
+  let gui: any;
+
+  const initGui = async () => {
+    const dat = await import("dat.gui");
+    gui = new dat.GUI();
+  };
+
+
   useEffect(() => {
-    const test = new SceneInit('myThreeJsCanvas');
-    test.initScene();
-    test.animate();
+    (
+      async () => {
+        const test = new SceneInit('myThreeJsCanvas');
+        test.initScene();
+        test.animate();
 
-    const p = new Piano();
-    test.scene.add(p.getPianoGroup());
+        const p = new Piano();
+        test.scene.add(p.getPianoGroup());
 
-    const fontLoader = new FontLoader();
-    fontLoader.load('../../public/fonts/Helvetica-Bold.typeface.json', (font) => {
-      p.renderText(font);
-    });
+        const fontLoader = new FontLoader();
+        fontLoader.load('./fonts/Helvetica-Bold.typeface.json', (font) => {
+          p.renderText(font);
+        });
 
-    const gui = new GUI();
-    const cameraFolder = gui.addFolder('Camera');
-    cameraFolder.add(test.camera.position, 'z', 100, 200);
-    cameraFolder.open();
+        await initGui();
+        // const gui = new GUI();
+        const cameraFolder = gui.addFolder('Camera');
+        cameraFolder.add(test.camera.position, 'z', 100, 200);
+        cameraFolder.open();
 
-    const pianoFolder = gui.addFolder('Piano');
-    pianoFolder.addColor(p, 'highlightColor').name('Highlight Color');
-    pianoFolder
-      .add(p, 'displayText')
-      .name('Display Text')
-      .onChange((value) => {
-        if (value) {
-          p.renderText();
-        } else {
-          p.hideText();
-        }
-      });
-    pianoFolder.open();
+        // NOTE: UI bug caused by importing tailwind css.
+        const pianoFolder = gui.addFolder('Piano');
+        pianoFolder.addColor(p, 'highlightColor').name('Highlight Color');
+        pianoFolder
+          .add(p, 'displayText')
+          .name('Display Text')
+          .onChange((value:any) => {
+            if (value) {
+              p.renderText();
+            } else {
+              p.hideText();
+            }
+          });
+        pianoFolder.open();
 
-    const onKeyDown = (event: any) => {
-      if (event.repeat) {
-        return;
+        const onKeyDown = (event:any) => {
+          if (event.repeat) {
+            return;
+          }
+          p.maybePlayNote(event.key);
+        };
+
+        const onKeyUp = (event:any) => {
+          p.maybeStopPlayingNote(event.key);
+        };
+
+        window.addEventListener('keyup', onKeyUp);
+        window.addEventListener('keydown', onKeyDown);
+
+        return () => {
+          window.removeEventListener('keyup', onKeyUp);
+          window.removeEventListener('keydown', onKeyDown);
+        };
       }
-      p.maybePlayNote(event.key);
-    };
+    )();
 
-    const onKeyUp = (event: any) => {
-      p.maybeStopPlayingNote(event.key);
-    };
-
-    window.addEventListener('keydown', onKeyDown);
-    window.addEventListener('keyup', onKeyUp);
-    return () => {
-      window.removeEventListener('keyup', onKeyUp);
-      window.removeEventListener('keydown', onKeyDown);
-    };
-
-  }, []);
+    // NOTE: Play piano with mouse.
+    // const mouse = new THREE.Vector2();
+    // const raycaster = new THREE.Raycaster();
+    // function onMouseDown(event) {
+    //   mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    //   mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    //   raycaster.setFromCamera(mouse, test.camera);
+    //   const intersects = raycaster.intersectObjects(pianoObject.children);
+    //   console.log(intersects);
+    //   if (intersects.length > 0) {
+    //     intersects.forEach((note) =>
+    //       console.log((note.object.position.z = -10))
+    //     );
+    //   }
+    // }
+  });
 
   return (
     <div>
       <canvas id="myThreeJsCanvas"></canvas>
     </div>
-  )
+  );
 }
 
 export default Home;
